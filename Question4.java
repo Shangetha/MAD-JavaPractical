4082f46bd7f54798867474cb37ebbd50aa38b615
westwq <31364147+westwq@users.noreply.github.com>
2020-03-24 13:51:47 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -14,7 +14,7 @@ public class Question4
      *     **
      *     *
      * 
-     * Hint: 2 loops are required
+     * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
     Scanner in = new Scanner(System.in);
