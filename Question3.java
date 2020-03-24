ab51b8675baa3e2e3092cea722872c2644403ff7
westwq <31364147+westwq@users.noreply.github.com>
2020-03-24 13:24:50 +0800

    Create Question3.java

---
+++ Question3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Question3
+{
+  public static void main(String[] args)
+  {
+    /**
+     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
+     * 
+     * Hint: in.nextDouble() to read double
+     */
+     
+    Scanner in = new Scanner(System.in);
+    
+  }
+}
