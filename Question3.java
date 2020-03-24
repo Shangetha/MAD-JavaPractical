96beb6deb3b96263cf2cbddfe055a3c0dc1791d4
westwq <31364147+westwq@users.noreply.github.com>
2020-03-24 14:08:13 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -7,7 +7,7 @@ public class Question3
     /**
      * Reads integer entered by the user, multiplies the integer by itself and print out the result.
      * 
-     * Hint: in.nextDouble() to read double
+     * Hint: in.nextInt() to read integer
      */
      
     Scanner in = new Scanner(System.in);
