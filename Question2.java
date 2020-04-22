a81160d34676a9225cc8a440e5c69f0e476e8cd7
westwq <31364147+westwq@users.noreply.github.com>
2020-04-22 14:37:07 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -5,7 +5,7 @@ public class Question2
   public static void main(String[] args)
   {
     /**
-     * Prompts user for height and weight, calculate and print out the height and weight of the user
+     * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
      * Hint: in.nextDouble() to read double
      */
