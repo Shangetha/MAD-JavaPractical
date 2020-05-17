a92d8c93152aed1b825fbbb92b60d813fd916fad
Shangetha <64067570+Shangetha@users.noreply.github.com>
2020-05-17 17:24:47 +0530

    completed

--- Question5.java
+++ Question5.java
@@ -27,6 +27,27 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
-    
+        int cnt[] = new int[100];
+		int i;
+        System.out.println("How many integers would you like to input(Max.100?)");
+		int x = in.nextInt();
+        System.out.println("Input the integers:");
+        for (i = 0; i <x; i++){ 
+         int n = in.nextInt();
+            cnt[--n]++;	
+		}
+		 int max = 0;
+        for (int n : cnt){
+            if (max < n){
+                max = n;
+            }
+        }
+         System.out.println("Mode value(s)in ascending order:");
+        for (i = 0; i < cnt.length; i++){
+            if (cnt[i] == max){
+                System.out.println(i + 1);
+            }
+        }
   }
+      
 }
