d15d70362a16aa6d1a679f5b5b5d1f3a2770fd67
Shangetha <64067570+Shangetha@users.noreply.github.com>
2020-05-17 17:04:58 +0530

    completed

--- Question3.java
+++ Question3.java
@@ -11,6 +11,10 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter number: ");
+    int num = in.nextInt();
+    int result = num*num;
+    System.out.print(result);
+
   }
 }
